fcb5fc344943e784eeb96c068ea32b213e74e78c
Per Liedman <per.liedman@dotnetmentor.se>
2017-11-10 19:16:51 +0100

    Fix test

--- test/index.test.js
+++ test/index.test.js
@@ -1,14 +1,14 @@
 const co2 = require('../src')
 
 test('can calculate transport emissions', () => {
-  const e = co2.transport(400)
+  const e = co2.fromTransport(400)
   expect(e).toBeDefined()
   expect(e.air).toBeDefined()
   expect(e.air.emissions).toBeGreaterThan(0)
 })
 
 test('can select transport modes', () => {
-  const e = co2.transport(400, {modes: []})
+  const e = co2.fromTransport(400, {modes: []})
   expect(e).toBeDefined()
   expect(e.air).toBeUndefined()
 })
